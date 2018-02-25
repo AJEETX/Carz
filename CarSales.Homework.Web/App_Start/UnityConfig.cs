1dfda519db5b27e3a944d0dbc3cf4a55d89604d0
Ajeet <ajeet.tiwari@helloworld.com.au>
2018-02-25 20:14:41 +1100

    updated nugets

--- CarSales.Homework.Web/App_Start/UnityConfig.cs
+++ CarSales.Homework.Web/App_Start/UnityConfig.cs
@@ -1,44 +1,48 @@
 using System;
-using Microsoft.Practices.Unity;
-using Microsoft.Practices.Unity.Configuration;
-using CarSales.Homework.Web.ServiceContract;
-using CarSales.Homework.Web.Service;
 using CarSales.Homework.Web.Repository;
+using CarSales.Homework.Web.Service;
+using CarSales.Homework.Web.ServiceContract;
+using Unity;
 
-namespace CarSales.Homework.Web.App_Start
+namespace CarSales.Homework.Web
 {
     /// <summary>
     /// Specifies the Unity configuration for the main container.
     /// </summary>
-    public class UnityConfig
+    public static class UnityConfig
     {
         #region Unity Container
-        private static Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() =>
-        {
-            var container = new UnityContainer();
-            RegisterTypes(container);
-            return container;
-        });
+        private static Lazy<IUnityContainer> container =
+          new Lazy<IUnityContainer>(() =>
+          {
+              var container = new UnityContainer();
+              RegisterTypes(container);
+              return container;
+          });
 
         /// <summary>
-        /// Gets the configured Unity container.
+        /// Configured Unity Container.
         /// </summary>
-        public static IUnityContainer GetConfiguredContainer()
-        {
-            return container.Value;
-        }
+        public static IUnityContainer Container => container.Value;
         #endregion
 
-        /// <summary>Registers the type mappings with the Unity container.</summary>
+        /// <summary>
+        /// Registers the type mappings with the Unity container.
+        /// </summary>
         /// <param name="container">The unity container to configure.</param>
-        /// <remarks>There is no need to register concrete types such as controllers or API controllers (unless you want to 
-        /// change the defaults), as Unity allows resolving a concrete type even if it was not previously registered.</remarks>
+        /// <remarks>
+        /// There is no need to register concrete types such as controllers or
+        /// API controllers (unless you want to change the defaults), as Unity
+        /// allows resolving a concrete type even if it was not previously
+        /// registered.
+        /// </remarks>
         public static void RegisterTypes(IUnityContainer container)
         {
-            // NOTE: To load from web.config uncomment the line below. Make sure to add a Microsoft.Practices.Unity.Configuration to the using statements.
+            // NOTE: To load from web.config uncomment the line below.
+            // Make sure to add a Unity.Configuration to the using statements.
             // container.LoadConfiguration();
 
-            //  Register your types here
+            // TODO: Register your type's mappings here.
             container.RegisterType<ICarService, CarService>();
             container.RegisterType<ICarRepository, CarRepository>();
         }
